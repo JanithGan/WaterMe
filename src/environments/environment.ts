ef972ff29a16af2890dfc301a055092201de339c
janithgan <janithgan@gmail.com>
2020-05-02 17:33:15 +0530

    Firebase Added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyCQe-R7EzhMkBTbGB5ycG_s1MpOJJfT52I",
+    authDomain: "waterme-66618.firebaseapp.com",
+    databaseURL: "https://waterme-66618.firebaseio.com",
+    projectId: "waterme-66618",
+    storageBucket: "waterme-66618.appspot.com",
+    messagingSenderId: "287106820710",
+    appId: "1:287106820710:web:8bd6cd08273bfdfd811ba7",
+    measurementId: "G-E0TJSVY8QR"
+  },
 };
 
 /*

82a87c0b8275c8d030a5afd5aaa3506202fba986
janithgan <janithgan@gmail.com>
2020-05-02 17:33:15 +0530

    Firebase Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { environment } from './../environments/environment';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +15,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCQe-R7EzhMkBTbGB5ycG_s1MpOJJfT52I",
+    authDomain: "waterme-66618.firebaseapp.com",
+    databaseURL: "https://waterme-66618.firebaseio.com",
+    projectId: "waterme-66618",
+    storageBucket: "waterme-66618.appspot.com",
+    messagingSenderId: "287106820710",
+    appId: "1:287106820710:web:8bd6cd08273bfdfd811ba7",
+    measurementId: "G-E0TJSVY8QR"
+  }
 };

d0620ca5870b14982843ca6601569124678b0e02
janithgan <janithgan@gmail.com>
2020-06-29 16:30:06 +0530

    sign up backend added

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,10 +1,11 @@
+import { DataService } from './data.service';
 import { AppUser } from '../models/app-user';
 import { UserService } from './user.service';
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Observable, of, empty } from 'rxjs';
 import { ActivatedRoute, Router } from '@angular/router';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, take } from 'rxjs/operators';
 import * as firebase from 'firebase/app';
 
 @Injectable({
@@ -14,29 +15,49 @@ export class AuthService {
 
   user$: Observable<firebase.User>;
 
-  constructor(private afAuth: AngularFireAuth, private route: ActivatedRoute, private userService: UserService, private router: Router) {
+  constructor(private afAuth: AngularFireAuth,
+    private route: ActivatedRoute,
+    private userService: UserService,
+    private dataService: DataService,
+    private router: Router) {
+
     this.user$ = afAuth.authState;
   }
 
-  signUp(email: string, password: string, deviceId: string) {
+  signUp(email: string, password: string, deviceId: string, devicePassword: string) {
+    this.dataService.get(deviceId).pipe(take(1)).subscribe(data => {
+      if (data) {
+        if (data["password"] == devicePassword) {
+          this.createAccount(email, password, deviceId);
+        }
+        else
+          alert("Device password does not match");
+      }
+      else
+        alert("No device with this ID")
+    });
+  }
+
+  createAccount(email: string, password: string, deviceId: string) {
     this.afAuth.createUserWithEmailAndPassword(email, password)
       .catch(function (error) {
         var errorCode = error.code;
         var errorMessage = error.message;
 
         if (errorCode == 'auth/weak-password') {
-          alert('The password is too weak.');
+          alert('The password is too weak');
         } else {
           alert(errorMessage);
         }
         console.log(error);
+      })
+      .then((value) => {
+        console.log(value["user"])
+        this.userService.save(value["user"], deviceId);;
       });
   }
 
   login(email: string, password: string) {
-    // let returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || '/';
-    // localStorage.setItem('returnUrl', returnUrl);
-
     this.afAuth.signInWithEmailAndPassword(email, password).catch(function (error) {
       var errorCode = error.code;
       var errorMessage = error.message;
@@ -44,7 +65,7 @@ export class AuthService {
       if (errorCode == 'auth/wrong-password') {
         alert('The password is wrong.');
       } else if (errorCode == 'auth/user-not-found') {
-        alert('The account does not exist');
+        alert('This account does not exist');
       } else {
         alert(errorMessage);
       }
@@ -64,5 +85,4 @@ export class AuthService {
       return of(null);
     }));
   }
-
 }
